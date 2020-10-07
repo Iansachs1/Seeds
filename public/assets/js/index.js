ea49501e81e9564f8a45c77a6f740018bad0ffbc
Francis Albuna <falbuna1@gmail.com>
2020-10-06 21:10:15 -0700

    Added reasons functionality.

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -2,10 +2,5 @@ $(document).ready(function(){
     // Write index page your CLIENT-SIDE logic here
     // This will run in the browser
 
-    $("#reasons").hide();
 });
 
-$("#bad").on("click", reasonsChoices);   function reasonsChoices(){
-    $("#badDay").show();
-    $("#dayQuality").hide();
-}
