6d15c6bdfdb1e679d1846b796db9da36480aa3f7
Francis Albuna <falbuna1@gmail.com>
2020-10-06 19:44:32 -0700

    Added bad day for reasons.

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
     // Write index page your CLIENT-SIDE logic here
     // This will run in the browser
+
+    $("#reasons").hide();
 });
+
+$("#bad").on("click", reasonsChoices);   function reasonsChoices(){
+    $("#badDay").show();
+    $("#dayQuality").hide();
+}
