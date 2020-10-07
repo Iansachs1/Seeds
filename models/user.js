2fee1d18098c9a179941d2bafb28dfaf9cae09a2
Iansachs1 <iansachs1@gmail.com>
2020-10-06 20:54:39 -0700

    added passport and bcrypt functionality

--- models/user.js
+++ models/user.js
@@ -1,22 +1,41 @@
-const db = require(".");
+// const db = require(".");
 
-module.exports = function(sequelize, DataTypes) {
-    var User = sequelize.define("User", {
-      // Giving the Author model a name of type STRING
-      name: DataTypes.STRING,
-      email: DataTypes.STRING,
-      password: DataTypes.STRING
+const bcrypt = require("bcryptjs");
+
+module.exports = function (sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    // Giving the Author model a name of type STRING
+    name: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+
+  User.associate = function (models) {
+    User.hasMany(models.Post, {
+      foreignKey: "user_id"
     });
-  
-    User.associate = function(models) {
-      User.hasMany(models.Post, {
-        foreignKey: "user_id"
-      });
-      User.hasMany(models.Reason, {
-        foreignKey: "user_id"
-      });
-    };
-  
-    return User;
+    User.hasMany(models.Reason, {
+      foreignKey: "user_id"
+    });
   };
-  
+
+  User.prototype.validPassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+  };
+  // Hooks are automatic methods that run during various phases of the User Model lifecycle
+  // In this case, before a User is created, we will automatically hash their password
+  User.addHook("beforeCreate", function (user) {
+    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+  });
+  return User;
+};
